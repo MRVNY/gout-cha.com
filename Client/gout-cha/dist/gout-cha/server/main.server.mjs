9b8e96e5324804e21a99592ae3a2cbfd673690d0
QY <marvinyao@icloud.com>
2024-07-08 13:54:47 +0200

    Build test

---
+++ Client/gout-cha/dist/gout-cha/server/main.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a}from"./chunk-JYK46NL5.mjs";import"./chunk-CJY4C4JN.mjs";import"./chunk-NDYDZJSS.mjs";export{a as default};
--- Server/obj/Debug/net8.0/Server.AssemblyInfo.cs
+++ Server/obj/Debug/net8.0/Server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c492aab500f35e1293c8272ed84ac07303091128")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+60da093ac79c58148e1621b53dbefb207c499136")]
 [assembly: System.Reflection.AssemblyProductAttribute("Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
