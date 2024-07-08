7b0388374e1b8ea23553c792895bf85edcf5ae9c
QY <marvinyao@icloud.com>
2024-07-08 21:50:00 +0200

    ??

--- Server/obj/Debug/net8.0/Server.AssemblyInfo.cs
+++ Server/obj/Debug/net8.0/Server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+60da093ac79c58148e1621b53dbefb207c499136")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6dc12df9b7dd2b923e0989b548171da844d106b8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
