05040576e3b802ee4da5570e1cffd4c5fd969e68
QY <marvinyao@icloud.com>
2024-07-09 13:50:37 +0200

    Server configured with Azure App Service

---
+++ Client/gout-cha/src/app/tea.ts
@@ -0,0 +1,5 @@
+export interface Tea {
+    name: string;
+    image: string;
+    description: string;
+}
--- Server/obj/Debug/net8.0/Server.AssemblyInfo.cs
+++ Server/obj/Debug/net8.0/Server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6dc12df9b7dd2b923e0989b548171da844d106b8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3b49bac6586fc57fae7cb1673fe5303f5a08c34b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
